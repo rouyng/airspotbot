84dedaa05df1a50df3163e0f174d07a27da2c05d
rouyng <54250428+rouyng@users.noreply.github.com>
2020-06-05 17:05:45 -0700

    create placeholder function and structure. create watchlist example csv

--- twitbot.py
+++ twitbot.py
@@ -1,16 +1,19 @@
 import tweepy
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
-
-# Create API object
-api = tweepy.API(auth)
-
-# Create a tweet
-api.update_status("Hello Tweepy")
-
 # TODO: write functions to generate spot tweets based on adsbexchange api data
 # TODO: write functions to pull supplementary information (registration, photos) for spots and add in tweet replies
 
 
+def initialize_twitter_api(ckey, csec, atkn, asec):
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(ckey, csec)
+    auth.set_access_token(atkn, asec)
+    # Create API object
+    api = tweepy.API(auth)
+    return api
+
+
+if __name__ == "__main__":
+        twapi = initialize_twitter_api("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+        # Create a tweet
+        twapi.update_status("Hello Tweepy")
