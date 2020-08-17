c8118e603bb25124d91c0a56cba0a511d673462a
rouyng <54250428+rouyng@users.noreply.github.com>
2020-08-16 20:26:49 -0700

    add descriptive message to csv read error

--- adsbget.py
+++ adsbget.py
@@ -114,22 +114,26 @@ class Spotter:
         with open(self.watchlist_path) as watchlist_file:
             csv_reader = csv.reader(watchlist_file, delimiter=',')
             for row in csv_reader:
-                if row[0] == 'Key':
-                    continue
-                else:
-                    if row[1] == 'RN':
-                        self.watchlist_rn[row[0]] = {'desc': row[3].strip(), 'img': row[4].strip()}
-                        logging.info(f'Added {row[0]} to reg num watchlist. Description: "{row[3]}", image: {row[4]}')
-                    elif row[1] == 'TC':
-                        if row[2].lower() == 'y':
-                            mil_only = True
-                        else:
-                            mil_only = False
-                        self.watchlist_tc[row[0]] = {'desc': row[3].strip(), 'img': row[4].strip(), 'mil_only': mil_only}
-                        logging.info(f'Added {row[0]} to type code watchlist. Military only: {mil_only} Description: "{row[3]}", image: {row[4]}')
-                    elif row[1] == 'IA':
-                        self.watchlist_ia[row[0]] = {'desc': row[3].strip(), 'img': row[4].strip()}
-                        logging.info(f'Added {row[0]} to ICAO address watchlist. Description: "{row[3]}", image: {row[4]}')
+                try:
+                    if row[0] == 'Key':
+                        continue
+                    else:
+                        if row[1] == 'RN':
+                            self.watchlist_rn[row[0]] = {'desc': row[3].strip(), 'img': row[4].strip()}
+                            logging.info(f'Added {row[0]} to reg num watchlist. Description: "{row[3]}", image: {row[4]}')
+                        elif row[1] == 'TC':
+                            if row[2].lower() == 'y':
+                                mil_only = True
+                            else:
+                                mil_only = False
+                            self.watchlist_tc[row[0]] = {'desc': row[3].strip(), 'img': row[4].strip(), 'mil_only': mil_only}
+                            logging.info(f'Added {row[0]} to type code watchlist. Military only: {mil_only} Description: "{row[3]}", image: {row[4]}')
+                        elif row[1] == 'IA':
+                            self.watchlist_ia[row[0]] = {'desc': row[3].strip(), 'img': row[4].strip()}
+                            logging.info(f'Added {row[0]} to ICAO address watchlist. Description: "{row[3]}", image: {row[4]}')
+                except IndexError as e:
+                    logging.error("Error reading watchlist.csv, please check the watchlist file.")
+                    raise e
             logging.info(f'Added {len(self.watchlist_rn) + len(self.watchlist_tc) + len(self.watchlist_ia)} entries to the watchlist')
 
     def _append_craft(self, aircraft):
