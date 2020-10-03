e737ba8c911f6227dccd21488fe73ef2ca5d4b9e
rouyng <54250428+rouyng@users.noreply.github.com>
2020-10-03 11:59:15 -0700

    create placeholders for pytest

---
+++ tests/test_location.py
@@ -0,0 +1,18 @@
+"""
+Tests for the location.py module
+"""
+
+from .context import airspotbot
+
+import pytest
+import sys
+
+
+class TestPlaceholder:
+    def test_our_testing(self):
+        """quick sanity check to make sure pytest is working as expected"""
+        assert True is not False
+
+    def test_import(self):
+        """Test whether module to be tested was successfully imported"""
+        assert "airspotbot.location" in sys.modules
