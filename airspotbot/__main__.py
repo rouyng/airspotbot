65c7e05a7e8e8885884238ad82954bb06ecd4363
rouyng <54250428+rouyng@users.noreply.github.com>
2022-03-03 11:08:41 -0700

    change config/watchlist arguments to optional flags, instead of positional

--- airspotbot/__main__.py
+++ airspotbot/__main__.py
@@ -1,23 +1,23 @@
 from . import airspotbot
 import argparse
 
-DEFAULT_CONFIG_PATH = 'config/asb.config'
-DEFAULT_WATCHLIST_PATH = 'config/watchlist.csv'
+DEFAULT_CONFIG_PATH = './config/asb.config'
+DEFAULT_WATCHLIST_PATH = './config/watchlist.csv'
 
 
 parser = argparse.ArgumentParser(description='A twitter bot for reporting aircraft activity in '
                                              'an area, using the ADS-B Exchange API. For more '
                                              'details, see README.md.',
                                  prog="airspotbot")
-parser.add_argument('config',
+parser.add_argument('-c', '--config',
                     type=str,
                     nargs='?',
-                    help=f'Path to config file. Default: {DEFAULT_CONFIG_PATH}',
+                    help=f'Path to config file. Optional, defaults to {DEFAULT_CONFIG_PATH}',
                     default=DEFAULT_CONFIG_PATH)
-parser.add_argument('watchlist',
+parser.add_argument('-w', '--watchlist',
                     type=str,
                     nargs='?',
-                    help=f'Path to watchlist file. Default: {DEFAULT_WATCHLIST_PATH}',
+                    help=f'Path to watchlist file. Optional, defaults to {DEFAULT_WATCHLIST_PATH}',
                     default=DEFAULT_WATCHLIST_PATH)
 
 # TODO: argument to disable twitter API connection and only print tweets to stdout
