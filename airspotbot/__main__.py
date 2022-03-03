ba0a03028238de4a8bc834af7619374701f447ff
rouyng <54250428+rouyng@users.noreply.github.com>
2022-03-03 11:31:03 -0700

    add TODO, change help message wording

--- airspotbot/__main__.py
+++ airspotbot/__main__.py
@@ -4,6 +4,8 @@ import argparse
 DEFAULT_CONFIG_PATH = './config/asb.config'
 DEFAULT_WATCHLIST_PATH = './config/watchlist.csv'
 
+# TODO: refactor logger to instantiate logging config once in this module, then use getLogger()
+#  in all other modules
 
 parser = argparse.ArgumentParser(description='A twitter bot for reporting aircraft activity in '
                                              'an area, using the ADS-B Exchange API. For more '
@@ -12,12 +14,12 @@ parser = argparse.ArgumentParser(description='A twitter bot for reporting aircra
 parser.add_argument('-c', '--config',
                     type=str,
                     nargs='?',
-                    help=f'Path to config file. Optional, defaults to {DEFAULT_CONFIG_PATH}',
+                    help=f'Optional path to config file. Defaults to {DEFAULT_CONFIG_PATH}',
                     default=DEFAULT_CONFIG_PATH)
 parser.add_argument('-w', '--watchlist',
                     type=str,
                     nargs='?',
-                    help=f'Path to watchlist file. Optional, defaults to {DEFAULT_WATCHLIST_PATH}',
+                    help=f'Optional path to watchlist file. Defaults to {DEFAULT_WATCHLIST_PATH}',
                     default=DEFAULT_WATCHLIST_PATH)
 
 # TODO: argument to disable twitter API connection and only print tweets to stdout
