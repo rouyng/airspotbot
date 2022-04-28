09688242b48a74c1a3afeb7f394c191f1e67dbe0
rouyng <54250428+rouyng@users.noreply.github.com>
2022-04-28 09:03:34 -0700

    add javascript snippet to hide ad banners in screenshot

--- airspotbot/screenshot.py
+++ airspotbot/screenshot.py
@@ -39,7 +39,7 @@ class Screenshotter:
         options = webdriver.ChromeOptions()  # create ChromeOptions object to set webdriver options
         options.add_argument('headless')  # set chrome webdriver to run in headless mode
         # TODO: modify window size to get all spatial information in the screenshot
-        options.add_argument('window-size=1200x600')
+        options.add_argument('window-size=1200x800')
         # The following two options help the webdriver play nice in Docker environment
         options.add_argument('disable-dev-shm-usage')
         options.add_argument('no-sandbox')
@@ -68,8 +68,18 @@ class Screenshotter:
              PNG screenshot as binary data
         """
         logger.debug(f"Getting browser screenshot for ICAO {icao}")
-        self.driver.get(f"https://globe.adsbexchange.com/?icao={icao}&zoom={self.zoom}&kiosk")
-        map_element = self.driver.find_element(by=By.CSS_SELECTOR, value="canvas.ol-layer")
-        sleep(1)  # sleep to let the page finish loading, otherwise a shaded grid overlay appears
+        self.driver.get(f"https://globe.adsbexchange.com/?icao={icao}&zoom={self.zoom}")
         # TODO: use a webdriver wait instead of sleep
+        sleep(1)  # sleep to let the page finish loading, otherwise a shaded grid overlay appears
+        # execute javascript to hide ad banners
+        self.driver.execute_script("""
+            var ad_selectors = ["#adsense",".adsbygoogle"];
+            for (var i=0;i<ad_selectors.length;i++) {
+                let ad_element = document.querySelector(ad_selectors[i])
+                if ( ad_element )
+                    ad_element.style.display = "none";
+            }
+        """)
+        map_element = self.driver.find_element(by=By.CSS_SELECTOR, value="canvas.ol-layer")
+
         return map_element.screenshot_as_png
